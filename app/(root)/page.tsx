ecb24ec422a81cf3ba256464b536a6d39cb5faf7
gguilhermelopes <guilhermelopes275@gmail.com>
2023-07-10 01:10:59 -0300

    chore: set global state to store creation modal and create provider to its render

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal isOpen onClose={() => {}} title="Título" description="Descrição">
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
