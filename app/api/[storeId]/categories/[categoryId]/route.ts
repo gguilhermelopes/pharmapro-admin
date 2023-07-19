404092b2fd501d874c325341ee62443201b753ae
gguilhermelopes <guilhermelopes275@gmail.com>
2023-07-18 23:27:13 -0300

    fix: include billboard on get category query

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -22,6 +22,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
